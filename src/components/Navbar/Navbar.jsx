3ea84bae29b073db2577ddb22596696b512c4a73
am-mahmud <am.mahmudasif@gmail.com>
2025-11-09 11:45:14 -0500

    Auth system updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { use } from 'react';
 import { AuthContext } from '../../contexts/AuthContext/AuthContext';
 import ActiveLink from '../ActiveLink/ActiveLink';
+import { RxHamburgerMenu } from 'react-icons/rx';
+
 
 const Navbar = () => {
 
@@ -39,26 +41,31 @@ const Navbar = () => {
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        <RxHamburgerMenu />
                     </div>
                     <ul
                         tabIndex="-1"
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li><a>Home</a></li>
-                        <li><a>Bills</a></li>
+                        {links}
                     </ul>
                 </div>
                 <a className="text-xl px-1 py-1 font-bold">USM</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Home</a></li>
-                    <li><a>Bills</a></li>
+                    {links}
                 </ul>
             </div>
-            <div className="navbar-end">
-                <a className="btn-primary-ui">Login</a>
-            </div>
+
+            {!user ? <Link to='/login'> <button className="btn-primary-ui">Login</button> </Link> : <div className=' flex items-center gap-1'> <Link to="/profile">
+                <img
+                    src={user.photoURL || "https://i.ibb.co.com/chgmm5K6/retro-game-9.jpg"}
+                    alt="Profile"
+                    className="w-10 h-10 rounded-lg border-2 border-black cursor-pointer"
+                />
+            </Link><Link to='/register'><button onClick={handleSignOut} className="btn-primary-ui">Logout</button></Link>
+
+            </div>}
         </div>
     );
 };
