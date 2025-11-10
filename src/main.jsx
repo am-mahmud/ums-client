76f804d09d0aa81cf7f0395549f7a29002d50ff2
am-mahmud <am.mahmudasif@gmail.com>
2025-11-09 19:17:30 -0500

    Auth deleted cause 400 issue

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import router from './routes/Routes';
 import { ToastContainer } from "react-toastify";
 
 import { RouterProvider } from "react-router/dom";
-import AuthProvider from './contexts/AuthProvider/AuthProvider';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
+   
       <RouterProvider router={router} />
-    </AuthProvider>
+   
 
     <ToastContainer
       position="top-center"
