efdb768583a4c839069da1999a75c108791fa85f
am-mahmud <am.mahmudasif@gmail.com>
2025-11-10 03:13:36 -0500

    Add bill updated

--- src/pages/AddBill/AddBill.jsx
+++ src/pages/AddBill/AddBill.jsx
@@ -31,7 +31,36 @@ const AddBill = () => {
                     <option value="Internet">Internet</option>
                 </select>
 
-                
+                <input
+                    type="number"
+                    name="amount"
+                    placeholder="Amount"
+                    required
+                    className="w-full p-3 border rounded"
+                />
+
+                <input
+                    type="text"
+                    name="location"
+                    placeholder="Location"
+                    required
+                    className="w-full p-3 border rounded"
+                />
+
+                <textarea
+                    name="description"
+                    placeholder="Description"
+                    required
+                    className="w-full p-3 border rounded"
+                ></textarea>
+
+                <input
+                    type="date"
+                    name="date"
+                    required
+                    className="w-full p-3 border rounded"
+                />
+
                 <button
                     type="submit"
                     className="w-full btn-primary-ui text-white p-3 rounded"
