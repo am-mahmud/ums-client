0551be165ca014a5b0dd430f06c9301b87ab01b8
am-mahmud <am.mahmudasif@gmail.com>
2025-11-10 08:10:04 -0500

    Bills page updated

--- src/pages/Bills/Bills.jsx
+++ src/pages/Bills/Bills.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import CategoryCard from '../../components/CategoryCard/CategoryCard';
 import AllBillCard from '../../components/AllBillCard/AllBillCard';
 
 const Bills = () => {
-    
+
     return (
         <div>
-            <h1 className='text-gray-800 font-bold text-3xl text-center mt-5 mb-5'>All Bills Till Today</h1>
-                <AllBillCard></AllBillCard>
+            <h1 className="text-gray-800 text-3xl font-bold text-center mt-5 mb-5">
+                All Bills Till Today
+            </h1>
+            <AllBillCard />
         </div>
     );
 };
