db248c5aebc8a98a6e293f28ff66a749aec58956
am-mahmud <am.mahmudasif@gmail.com>
2025-11-11 04:05:12 -0500

    Builin toggle button added from uivese

--- src/components/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -6,13 +6,13 @@ const LoadingSpinner = () => {
 
             <div className="w-full gap-x-2 flex justify-center items-center">
                 <div
-                    className="w-5 bg-[#2A7B9B] animate-pulse h-5 rounded-full animate-bounce"
+                    className="w-5 bg-[#2A7B9B] h-5 rounded-full animate-bounce"
                 ></div>
                 <div
-                    className="w-5 animate-pulse h-5 bg-[#57C785] rounded-full animate-bounce"
+                    className="w-5 h-5 bg-[#57C785] rounded-full animate-bounce"
                 ></div>
                 <div
-                    className="w-5 h-5 animate-pulse bg-[#EDDD53] rounded-full animate-bounce"
+                    className="w-5 h-5  bg-[#EDDD53] rounded-full animate-bounce"
                 ></div>
             </div>
 
