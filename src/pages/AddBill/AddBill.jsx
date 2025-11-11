da69befa36befaa9ffef867c5d3280d89201956c
am-mahmud <am.mahmudasif@gmail.com>
2025-11-10 22:29:07 -0500

    Add bill ui updated

--- src/pages/AddBill/AddBill.jsx
+++ src/pages/AddBill/AddBill.jsx
@@ -1,9 +1,19 @@
 import { div } from 'framer-motion/client';
-import React, { useState } from 'react';
+import React, { use, useEffect, useState } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
 
 const AddBill = () => {
   const [loading, setLoading] = useState(false);
   const [added, setAdded] = useState(false);
+  const { user } = use(AuthContext)
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data))
+      .catch((err) => console.error("Error fetching categories", err));
+  }, []);
 
   const handleAddBill = (e) => {
     e.preventDefault();
@@ -11,15 +21,20 @@ const AddBill = () => {
     setAdded(false);
 
     const form = e.target;
+    const title = form.title.value;
+    const category = form.category.value;
+    const amount = parseFloat(form.amount.value);
+    const description = form.description.value;
+    const date = new Date();
 
     const newBill = {
-      billsId: form.billsId.value,
-      username: form.username.value,
-      phone: form.phone.value,
-      address: form.address.value,
-      email: form.email.value,
-      amount: Number(form.amount.value),
-      date: form.date.value,
+      title,
+      category,
+      amount,
+      description,
+      date,
+      image: "https://i.ibb.co.com/Y4VwJkNw/thunder.png",
+      createdBy: user?.email,
     };
 
     fetch("http://localhost:3000/bills", {
@@ -47,62 +62,35 @@ const AddBill = () => {
       <div className="max-w-xl mx-auto py-10 text-center ">
         <h1 className="text-3xl font-bold">Add New Bill</h1>
 
-        <form onSubmit={handleAddBill} className="mt-6 grid grid-cols-1 md:grid-cols-2 gap-4">
+        <form onSubmit={handleAddBill} className="mt-6">
+    
+          <div>
+            <label className="block text-start mb-1 font-medium">Bill Title</label>
+            <input name="title" type="text" required className="w-full border p-2 rounded" />
+          </div>
 
-          <input
-            type="text"
-            name="billsId"
-            placeholder="Bill ID (e.g. abc123)"
-            required
-            className="w-full p-3 border rounded"
-          />
+          <div>
+            <label className="block text-start mb-1 font-medium">Category</label>
+            <select name="category" required className="w-full border p-2 rounded">
+              <option value="">Select Category</option>
+              {categories.map((cat) => (
+                <option key={cat._id} value={cat.title}>
+                  {cat.title}
+                </option>
+              ))}
+            </select>
+          </div>
 
-          <input
-            type="text"
-            name="username"
-            placeholder="Username"
-            required
-            className="w-full p-3 border rounded"
-          />
+          <div>
+            <label className="block text-start mb-1 font-medium">Amount ($)</label>
+            <input name="amount" type="number" step="0.01" required className="w-full border p-2 rounded" />
+          </div>
 
-          <input
-            type="tel"
-            name="phone"
-            placeholder="Phone Number"
-            required
-            className="w-full p-3 border rounded"
-          />
+          <div>
+            <label className="block  text-start mb-1 font-medium">Description</label>
+            <textarea name="description" rows="3" required className="w-full border p-2 rounded"></textarea>
+          </div>
 
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            required
-            className="w-full p-3 border rounded"
-          />
-
-          <input
-            type="text"
-            name="address"
-            placeholder="Address"
-            required
-            className="w-full p-3 border rounded md:col-span-2"
-          />
-
-          <input
-            name="amount"
-            type="number"
-            placeholder="Amount"
-            required
-            className="p-3 border rounded md:col-span-2"
-          />
-
-          <input
-            name="date"
-            type="date"
-            required
-            className="p-3 border rounded md:col-span-2"
-          />
 
           <div className="md:col-span-2">
             <button
