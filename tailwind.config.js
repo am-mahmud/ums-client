d275b70567d7b3acf98312c30ce7a9a55d654050
am-mahmud <am.mahmudasif@gmail.com>
2025-11-13 06:30:47 -0500

    Escape warning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
+// export default {
+//   darkMode: "class", 
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+/** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class", 
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'stack-sans': ['"Stack Sans Headline"', 'sans-serif'],
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
 };
