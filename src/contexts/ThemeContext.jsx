b123120ed405dda11c3601cb92f3130cbdeeac69
am-mahmud <am.mahmudasif@gmail.com>
2025-11-13 05:10:22 -0500

    Theme function update

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,9 +1,43 @@
-import React, { createContext, useState } from "react";
+// import React, { createContext, useState } from "react";
+
+// export const ThemeContext = createContext();
+
+// export const ThemeProvider = ({ children }) => {
+//   const [isDark, setIsDark] = useState(false);
+
+//   const toggleTheme = () => setIsDark((prev) => !prev);
+
+//   return (
+//     <ThemeContext.Provider value={{ isDark, toggleTheme }}>
+//       {children}
+//     </ThemeContext.Provider>
+//   );
+// };
+
+
+import React, { createContext, useState, useEffect } from "react";
 
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [isDark, setIsDark] = useState(false);
+  // Get theme from localStorage if exists or default to light - to prevent default os theme mode
+  const [isDark, setIsDark] = useState(() => {
+    const saved = localStorage.getItem("theme");
+    return saved ? JSON.parse(saved) : false;
+  });
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+
+    if (isDark) {
+      root.classList.add("dark");
+    } else {
+      root.classList.remove("dark");
+    }
+
+    // Save preference in local storage
+    localStorage.setItem("theme", JSON.stringify(isDark));
+  }, [isDark]);
 
   const toggleTheme = () => setIsDark((prev) => !prev);
 
