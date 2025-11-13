4c5239311f9a3219dc1d5d7f9a5a9c86ec7927ef
am-mahmud <am.mahmudasif@gmail.com>
2025-11-13 06:32:52 -0500

    No escape from wanring

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,25 @@
-// export default {
-//   darkMode: "class", 
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,jsx,ts,tsx}",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// };
-
-/** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: "class", 
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {
-      fontFamily: {
-        'stack-sans': ['"Stack Sans Headline"', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
-  plugins: [require('daisyui')],
+  plugins: [],
 };
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   darkMode: 'class',
+//   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         'stack-sans': ['"Stack Sans Headline"', 'sans-serif'],
+//       },
+//     },
+//   },
+//   plugins: [require('daisyui')],
+// };
